76908d10c122686c68524ff0e65e2107f0f8b288
Yuchan Jeong <uchany25@gmail.com>
2025-11-04 01:39:30 +0900

    Swirl

---
+++ 9. Developing Data Products/My_First_Course/Swirlify.R
@@ -0,0 +1,28 @@
+library(swirlify)
+
+### Lesson 1
+new_lesson("Lesson 1", "My First Course")
+
+wq_message()
+wq_command()
+add_to_manifest()
+test_lesson() #If there is no error, the lesson works
+
+demo_lesson()
+
+
+### Lesson 2
+new_lesson("Lesson 2", "My First Course")
+
+wq_multiple() # Multiple choice type question
+add_to_manifest()
+
+test_lesson()
+
+demo_lesson()
+
+wq_figure() # figure type question
+
+wq_figure()
+
+demo_lesson()
